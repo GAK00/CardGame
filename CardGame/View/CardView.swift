c6ba59b20b182cc1ab005cc9f5f7aad0d9c18e6f
GAK00 <geran.kunz@gmail.com>
2016-11-21 09:53:27 -0700

    started work on CardView

---
+++ CardGame/View/CardView.swift
@@ -0,0 +1,35 @@
+//
+//  CardView.swift
+//  CardGame
+//
+//  Created by Kunz, Geran on 11/21/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class CardView
+{
+    internal var isFaceUp : Bool
+    internal var cardBack : UIImageView
+    internal var cardFront : UIImageView
+    internal var topLablel : UILabel
+    internal var bottomLabel : UILabel
+    internal var cardCurve : CGFloat
+    
+    init()
+    {
+        isFaceUp = false
+        cardBack = UIImageView()
+        cardFront = UIImageView()
+        topLablel = UILabel()
+        bottomLabel = UILabel()
+        cardCurve = CGFloat(20.00)
+        setup()
+    }
+    
+    func setup()
+    {
+      
+    }
+}
